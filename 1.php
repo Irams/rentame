481d89b1400a5ad6f21412bf20bfc9a843407225
Irams <herber.medina@gmail.com>
2019-09-24 16:31:04 -0500

    first commit

---
+++ 1.php
@@ -0,0 +1,75 @@
+<?php include_once 'includes/templates/header.php';?>
+		<div id="fh5co-main">
+
+			<div class="fh5co-narrow-content">
+				<div class="row">
+
+					<div class="col-md-12 animate-box" data-animate-effect="fadeInLeft">
+						<figure class="text-center">
+							<img src="images/work.jpg" alt="Free HTML5 Bootstrap" class="img-responsive">
+						</figure>
+					</div>
+					
+					<div class="col-md-8 col-md-offset-2 animate-box" data-animate-effect="fadeInLeft">
+						
+						<div class="col-md-9 col-md-push-3">
+							<h1>Vibrocompactador Raygo</h1>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+
+							<p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+						</div>
+
+						<!-- <div class="col-md-3 col-md-pull-9 fh5co-services">
+							<h3></h3>
+							<ul>
+								<li>Web Design</li>
+								<li>e-Commerce</li>
+								<li>Logo &amp; Branding</li>
+								<li>Packaging</li>
+							</ul>
+						</div> -->
+
+					</div>
+				</div>
+
+				<div class="row work-pagination animate-box" data-animate-effect="fadeInLeft">
+					<div class="col-md-8 col-md-offset-2 col-sm-12 col-sm-offset-0">
+
+						<div class="col-md-4 col-sm-4 col-xs-4 text-center">
+							<a href="#"><i class="icon-long-arrow-left"></i> <span>Previous Project</span></a>
+						</div>
+						<div class="col-md-4 col-sm-4 col-xs-4 text-center">
+							<a href="#"><i class="icon-th-large"></i></a>
+						</div>
+						<div class="col-md-4 col-sm-4 col-xs-4 text-center">
+							<a href="#"><span>Next Project</span> <i class="icon-long-arrow-right"></i></a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	
+	
+	<!-- MAIN JS -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,173 @@
+<?php include_once 'includes/templates/header.php';?>
+		<div id="fh5co-main">
+
+			<div class="fh5co-narrow-content">
+				<div class="row">
+					<div class="col-md-5">
+						<h2 class="fh5co-heading animate-box" data-animate-effect="fadeInLeft"><span>Nosotros</span></h2>
+						<p class="fh5co-lead animate-box" data-animate-effect="fadeInLeft">¡Contamos con todo lo necesario para su OBRA!. Con nosotros podrás encontrar la maquinaria que necesitas.</p>
+					</div>
+					<div class="col-md-6 col-md-push-1 animate-box" data-animate-effect="fadeInLeft">
+						<img src="images/img_1.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive"> 
+					</div>
+				</div>
+				<div class="row text-nosotros">
+					<div class="col-md-12">
+							<p class="animate-box" data-animate-effect="fadeInLeft"> Nos acoplamos a todo tipo de planes:
+							por día, 7, 15, 30 días o de acuerdo a tus necesidades.<br>
+							Somos una empresa con más 20 años de experiencia en el ramo de la construcción, dedicada a la Venta y Renta de Maquinaria.
+							Ofrecemos nuestros servicios en toda la República Mexicana.</p>
+							<p class="animate-box" data-animate-effect="fadeInLeft">Nuestros equipos son de las mejores marcas y la mejor calidad, ofrecemos costos accesibles para cualquier tipo de obra.</p>
+							<p class="animate-box" data-animate-effect="fadeInLeft">Todo nuestro personal se encuentra altamente capacitado para brindarle toda la atención que usted necesite al momento de rentar alguno de nuestros equipos.</p>
+							<p class="animate-box" data-animate-effect="fadeInLeft">Nuestro personal cuenta con Constancia de competencias o de habilidades laborales DC-3  SEGURIDAD PARA TRABAJOS EN ALTURAS por parte de la Secretaría del Trabajo.</p>
+					</div>
+				</div>
+			</div>
+
+			<!-- <div class="fh5co-narrow-content  animate-box" data-animate-effect="fadeInLeft">
+				<h2 class="fh5co-heading" >The Team</span></h2>
+
+				<div class="row">
+					<div class="col-md-4 fh5co-staff">
+						<img src="images/person3.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Athan Smith</h3>
+						<h4>Co-Founder, CEO</h4>
+						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="images/person2.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Nathalie Kosley</h3>
+						<h4>Co-Founder, CTO</h4>
+						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="images/person4.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Yanna Kuzuki</h3>
+						<h4>Co-Founder, Principal</h4>
+						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div> -->
+
+			<div class="fh5co-counters" style="background-image: url(images/hero.jpg);" data-stellar-background-ratio="0.5" id="counter-animate">
+				<div class="fh5co-narrow-content animate-box">
+					<div class="row" >
+						<div class="col-md-4 text-center animate-box">
+							<span class="fh5co-counter js-counter" data-from="0" data-to="235" data-speed="5000" data-refresh-interval="50"></span>
+							<span class="fh5co-counter-label">Rentas de equipos</span>
+						</div>
+						<div class="col-md-4 text-center animate-box">
+							<span class="fh5co-counter js-counter" data-from="0" data-to="520" data-speed="5000" data-refresh-interval="50"></span>
+							<span class="fh5co-counter-label">Kilómetros reencarpetados</span>
+						</div>
+						<div class="col-md-4 text-center animate-box">
+							<span class="fh5co-counter js-counter" data-from="0" data-to="20" data-speed="2500" data-refresh-interval="50"></span>
+							<span class="fh5co-counter-label">Años de experiencia</span>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- <div class="fh5co-narrow-content fh5co-border-bottom">
+				<h2 class="fh5co-heading animate-box" data-animate-effect="fadeInLeft">This Is What <span>We Love To Do</span></h2>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+							<div class="fh5co-icon">
+								<i class="icon-strategy"></i>
+							</div>
+							<div class="fh5co-text">
+								<h3>Strategy</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+							<div class="fh5co-icon">
+								<i class="icon-telescope"></i>
+							</div>
+							<div class="fh5co-text">
+								<h3>Explore</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6">
+						<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+							<div class="fh5co-icon">
+								<i class="icon-circle-compass"></i>
+							</div>
+							<div class="fh5co-text">
+								<h3>Direction</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+							<div class="fh5co-icon">
+								<i class="icon-tools-2"></i>
+							</div>
+							<div class="fh5co-text">
+								<h3>Expertise</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		
+
+			<div class="fh5co-narrow-content">
+				<div class="row">
+					<div class="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+						<h1 class="fh5co-heading-colored">Start Your Project Now!</h1>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+						<p class="fh5co-lead">Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+						<p><a href="#" class="btn btn-primary btn-outline">Get Started</a></p>
+					</div>
+					<div class="col-md-7 col-md-push-1">
+						<div class="row">
+							<div class="col-md-6 animate-box" data-animate-effect="fadeInLeft">
+								<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+							</div>
+							<div class="col-md-6 animate-box" data-animate-effect="fadeInLeft">
+								<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div> -->
+
+	<?php include_once 'includes/templates/footer.php';?>
+
